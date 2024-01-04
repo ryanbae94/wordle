e1b2e30c04480ac35f18b1c5f935466500670cb9
ryanbae94 <ryanbae94@gmail.com>
2024-01-05 01:12:09 +0900

    style

--- src/component/modal/InfoModal.tsx
+++ src/component/modal/InfoModal.tsx
@@ -15,7 +15,7 @@ export default function InfoModal({ isOpen, onClose }: ModalProps) {
 	return (
 		<div className='fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center'>
 			<div
-				className='inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'
+				className='inline-block align-bottom w-10/12 bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6'
 				onClick={handleModalClick}
 			>
 				<div className='absolute top-4 right-4'>
