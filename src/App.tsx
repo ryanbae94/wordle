3c2c06949c289a917f34bdb81ae30996b5838939
ryanbae94 <ryanbae94@gmail.com>
2024-06-03 20:10:47 +0900

    [feat] AI 대결 모드 로직

--- src/App.tsx
+++ src/App.tsx
@@ -6,213 +6,286 @@ import { ALL_WORDS, HARD_WORDS, EASY_WORDS } from './const/5words';
 import toast, { Toaster } from 'react-hot-toast';
 import { InfoModal } from './component/modal';
 import { searchWord } from './api/search';
+import { aiGuess } from './lib/aiBattle';
+import AiWinModal from './component/modal/AiWinModal';
 const LoseModal = React.lazy(() => import('./component/modal/LoseModal'));
 const WinModal = React.lazy(() => import('./component/modal/WinModal'));
 
 function App() {
-  const [answer, setAnswer] = useState(
-    EASY_WORDS[Math.floor(Math.random() * EASY_WORDS.length)],
-  );
-  const [hardMode, setHardMode] = useState(false);
-  const [currentRow, setCurrentRow] = useState(0);
-  const [currentColumn, setCurrentColumn] = useState(0);
-  const [guess, setGuess] = useState('');
-  const [isWinModalOpen, setIsWinModalOpen] = useState(false);
-  const [isLoseModalOpen, setIsLoseModalOpen] = useState(false);
-  const [isHelpModalOpen, setIsHelpModalOpen] = useState(true);
-  const [cellValues, setCellValues] = useState<
-    { letter: string; color: string }[][]
-  >(
-    Array(6)
-      .fill(null)
-      .map(() => Array(5).fill({ letter: '', color: '' })),
-  );
+	const [answer, setAnswer] = useState(
+		EASY_WORDS[Math.floor(Math.random() * EASY_WORDS.length)]
+	);
+	const [hardMode, setHardMode] = useState(false);
+	const [aiMode, setAiMode] = useState(true);
+	const [currentRow, setCurrentRow] = useState(0);
+	const [currentColumn, setCurrentColumn] = useState(0);
+	const [guess, setGuess] = useState('');
+	const [isWinModalOpen, setIsWinModalOpen] = useState(false);
+	const [isLoseModalOpen, setIsLoseModalOpen] = useState(false);
+	const [isHelpModalOpen, setIsHelpModalOpen] = useState(true);
+	const [isAiWinModalOpen, setIsAiWinModalOpen] = useState(false);
+	const [turn, setTurn] = useState(1);
+	const [cellValues, setCellValues] = useState<
+		{ letter: string; color: string }[][]
+	>(
+		Array(6)
+			.fill(null)
+			.map(() => Array(5).fill({ letter: '', color: '' }))
+	);
 
-  const fetchAnswer = async () => {
-    const result = await searchWord(answer);
-    console.log(result);
-  };
+	const fetchAnswer = async () => {
+		const result = await searchWord(answer);
+		console.log(result);
+	};
 
-  // 게임이 시작할 때 마다 단어 뜻 미리 검색
-  useEffect(() => {
-    fetchAnswer();
-  }, [answer]);
+	// 게임이 시작할 때 마다 단어 뜻 미리 검색
+	// useEffect(() => {
+	// 	fetchAnswer();
+	// }, [answer]);
 
-  useEffect(() => {
-    console.log(answer);
-    console.log('mode: ', hardMode);
-  }, [answer, hardMode]);
+	useEffect(() => {
+		console.log(answer);
+		console.log('mode: ', hardMode);
+	}, [answer, hardMode]);
 
-  const notWordToast = () => {
-    toast('올바른 단어가 아닙니다.\n다른 단어를 입력해 보세요.', {
-      duration: 2000,
-      style: {
-        backgroundColor: '#f05650',
-      },
-    });
-  };
+	const notWordToast = () => {
+		toast('올바른 단어가 아닙니다.\n다른 단어를 입력해 보세요.', {
+			duration: 2000,
+			style: {
+				backgroundColor: '#f05650',
+			},
+		});
+	};
 
-  const notFiveCharToast = () => {
-    toast('단어는 5글자로 입력해 주세요.', {
-      duration: 2000,
-      style: {
-        backgroundColor: '#ffff99',
-      },
-    });
-  };
+	const notFiveCharToast = () => {
+		toast('단어는 5글자로 입력해 주세요.', {
+			duration: 2000,
+			style: {
+				backgroundColor: '#ffff99',
+			},
+		});
+	};
 
-  const handleMode = () => {
-    const newMode = !hardMode;
-    setHardMode(newMode);
-    resetGame(newMode);
-  };
+	const handleMode = () => {
+		const newMode = !hardMode;
+		setHardMode(newMode);
+		resetGame(newMode);
+	};
 
-  const openWinModal = (answer: string) => {
-    setIsWinModalOpen(true);
-  };
+	const openWinModal = (answer: string) => {
+		setIsWinModalOpen(true);
+	};
 
-  const closeWinModalAndResetGame = () => {
-    setIsWinModalOpen(false);
-    resetGame(hardMode);
-  };
-  const openLoseModal = (answer: string) => {
-    setIsLoseModalOpen(true);
-  };
+	const closeWinModalAndResetGame = () => {
+		setIsWinModalOpen(false);
+		resetGame(hardMode);
+	};
+	const openLoseModal = (answer: string) => {
+		setIsLoseModalOpen(true);
+	};
 
-  const closeLoseModalAndResetGame = () => {
-    setIsLoseModalOpen(false);
-    resetGame(hardMode);
-  };
+	const closeLoseModalAndResetGame = () => {
+		setIsLoseModalOpen(false);
+		resetGame(hardMode);
+	};
 
-  const openHelpModal = () => {
-    setIsHelpModalOpen(true);
-  };
-  const closeHelpModal = () => {
-    setIsHelpModalOpen(false);
-  };
+	const openAiWinModal = (answer: string) => {
+		setIsAiWinModalOpen(true);
+	};
 
-  const resetGame = (mode: boolean) => {
-    setCurrentRow(0);
-    setCurrentColumn(0);
-    setGuess('');
-    setCellValues(
-      Array(6)
-        .fill(null)
-        .map(() => Array(5).fill({ letter: '', color: '' })),
-    );
-    const newAnswer = mode
-      ? HARD_WORDS[Math.floor(Math.random() * HARD_WORDS.length)]
-      : EASY_WORDS[Math.floor(Math.random() * EASY_WORDS.length)];
-    setAnswer(newAnswer);
-  };
+	const closeAiWinModalAndResetGame = () => {
+		setIsAiWinModalOpen(false);
+		resetGame(hardMode);
+	};
 
-  const isGameEnd = () => {
-    if (guess === answer) {
-      openWinModal(answer);
-    }
-    if (currentRow === 5 && guess !== answer) {
-      openLoseModal(answer);
-    }
-  };
+	const openHelpModal = () => {
+		setIsHelpModalOpen(true);
+	};
+	const closeHelpModal = () => {
+		setIsHelpModalOpen(false);
+	};
 
-  const guessing = async () => {
-    setCellValues((prev) => {
-      const newValues = [...prev];
+	const resetGame = (mode: boolean) => {
+		setCurrentRow(0);
+		setCurrentColumn(0);
+		setTurn(1);
+		setGuess('');
+		setCellValues(
+			Array(6)
+				.fill(null)
+				.map(() => Array(5).fill({ letter: '', color: '' }))
+		);
+		const newAnswer = mode
+			? HARD_WORDS[Math.floor(Math.random() * HARD_WORDS.length)]
+			: EASY_WORDS[Math.floor(Math.random() * EASY_WORDS.length)];
+		setAnswer(newAnswer);
+	};
 
-      for (let i = 0; i < guess.length; i++) {
-        if (guess[i] === answer[i]) {
-          newValues[currentRow][i].color = 'green';
-        } else if (answer.includes(guess[i])) {
-          newValues[currentRow][i].color = 'yellow';
-        } else {
-          newValues[currentRow][i].color = 'gray';
-        }
-      }
+	const isGameEnd = () => {
+		if (guess === answer) {
+			openWinModal(answer);
+		}
+		if (currentRow === 5 && guess !== answer) {
+			openLoseModal(answer);
+		}
+	};
 
-      return newValues;
-    });
+	const guessing = async () => {
+		setCellValues((prev) => {
+			const newValues = [...prev]; // 각 행을 개별적으로 복사
 
-    setTimeout(() => {
-      isGameEnd();
-      setCurrentRow(Math.min(currentRow + 1, 6));
-      setGuess('');
-      setCurrentColumn(0);
-    }, 0);
-  };
+			for (let i = 0; i < guess.length; i++) {
+				if (guess[i] === answer[i]) {
+					newValues[currentRow][i].color = 'green';
+				} else if (answer.includes(guess[i])) {
+					newValues[currentRow][i].color = 'yellow';
+				} else {
+					newValues[currentRow][i].color = 'gray';
+				}
+			}
 
-  const handleKeyPress = (key: string) => {
-    if (key === 'del') {
-      if (currentColumn >= 1) {
-        setCellValues((prev) => {
-          const newValues = [...prev];
-          if (currentColumn > 0) {
-            newValues[currentRow][currentColumn - 1].letter = '';
-            setCurrentColumn(Math.max(currentColumn - 1, 0));
-            setGuess(guess.slice(0, -1));
-          }
-          return newValues;
-        });
-      }
-    } else if (key === 'enter') {
-      if (currentColumn === 5) {
-        console.log('guess: ', guess);
+			return newValues;
+		});
 
-        if (!ALL_WORDS.includes(guess)) {
-          notWordToast();
-          return;
-        }
-        guessing();
-      } else {
-        notFiveCharToast();
-        return;
-      }
-    } else {
-      if (currentColumn < 5) {
-        setCellValues((prev) => {
-          const newValues = [...prev];
-          if (currentColumn < 5) {
-            const newRow = [...newValues[currentRow]];
-            newRow[currentColumn] = { letter: key, color: '' };
-            newValues[currentRow] = newRow;
-            setGuess(guess + key);
-            setCurrentColumn(Math.min(currentColumn + 1, 5));
-          }
-          return newValues;
-        });
-      }
-    }
-  };
+		setTimeout(() => {
+			isGameEnd();
+			setCurrentRow(Math.min(currentRow + 1, 6));
+			setGuess('');
+			setCurrentColumn(0);
+			setTurn(turn + 1);
+		}, 0);
+	};
 
-  return (
-    <div className="flex flex-col h-dvh md:h-screen gap-3 justify-between">
-      <Toaster
-        containerStyle={{
-          top: 100,
-        }}
-      />
-      <Header onHelpClick={openHelpModal} />
-      <GameBoard
-        cellValues={cellValues}
-        mode={hardMode}
-        switchMode={handleMode}
-      />
+	const aiGuessing = async () => {
+		const aiGuesses = aiGuess(hardMode, turn, cellValues);
+		console.log('aiGuesses: ', aiGuesses);
+		if (aiGuesses) {
+			setCellValues((prev) => {
+				const newValues = prev.map((row, rowIndex) => {
+					// 현재 행은 깊은 복사하고, 다른 행은 그대로 유지
+					return rowIndex === currentRow ? [...row] : row;
+				});
 
-      <Keyboard onKeyPress={handleKeyPress} cellValues={cellValues} />
-      <WinModal
-        isOpen={isWinModalOpen}
-        onClose={closeWinModalAndResetGame}
-        answer={answer}
-      />
-      <LoseModal
-        isOpen={isLoseModalOpen}
-        onClose={closeLoseModalAndResetGame}
-        answer={answer}
-      />
-      {isHelpModalOpen && (
-        <InfoModal isOpen={isHelpModalOpen} onClose={closeHelpModal} />
-      )}
-    </div>
-  );
+				for (let i = 0; i < aiGuesses.length; i++) {
+					if (aiGuesses[i] === answer[i]) {
+						newValues[currentRow][i] = {
+							...newValues[currentRow][i],
+							letter: aiGuesses[i],
+							color: 'green',
+						};
+					} else if (answer.includes(aiGuesses[i])) {
+						newValues[currentRow][i] = {
+							...newValues[currentRow][i],
+							letter: aiGuesses[i],
+							color: 'yellow',
+						};
+					} else {
+						newValues[currentRow][i] = {
+							...newValues[currentRow][i],
+							letter: aiGuesses[i],
+							color: 'gray',
+						};
+					}
+				}
+				return newValues;
+			});
+			isAiWin(aiGuesses);
+			setCurrentRow((prev) => Math.min(prev + 1, 6));
+			setGuess('');
+			setCurrentColumn(0);
+			setTurn((prev) => prev + 1);
+		}
+	};
+
+	const isAiWin = (aiGuesses: string) => {
+		if (aiGuesses === answer) {
+			openAiWinModal(aiGuesses);
+		}
+	};
+
+	useEffect(() => {
+		if (aiMode && hardMode && turn % 2 === 1 && turn < 6) {
+			aiGuessing();
+		}
+	}, [aiMode, hardMode, turn, cellValues, answer, currentRow]);
+
+	const handleKeyPress = (key: string) => {
+		if (key === 'del') {
+			if (currentColumn >= 1) {
+				setCellValues((prev) => {
+					const newValues = [...prev];
+					if (currentColumn > 0) {
+						newValues[currentRow][currentColumn - 1].letter = '';
+						setCurrentColumn(Math.max(currentColumn - 1, 0));
+						setGuess(guess.slice(0, -1));
+					}
+					return newValues;
+				});
+			}
+		} else if (key === 'enter') {
+			if (currentColumn === 5) {
+				console.log('guess: ', guess);
+
+				if (!ALL_WORDS.includes(guess)) {
+					notWordToast();
+					return;
+				}
+				guessing();
+			} else {
+				notFiveCharToast();
+				return;
+			}
+		} else {
+			if (currentColumn < 5) {
+				setCellValues((prev) => {
+					const newValues = [...prev];
+					if (currentColumn < 5) {
+						const newRow = [...newValues[currentRow]];
+						newRow[currentColumn] = { letter: key, color: '' };
+						newValues[currentRow] = newRow;
+						setGuess(guess + key);
+						setCurrentColumn(Math.min(currentColumn + 1, 5));
+					}
+					return newValues;
+				});
+			}
+		}
+	};
+
+	return (
+		<div className='flex flex-col h-dvh md:h-screen gap-3 justify-between'>
+			<Toaster
+				containerStyle={{
+					top: 100,
+				}}
+			/>
+			<Header onHelpClick={openHelpModal} />
+			<GameBoard
+				cellValues={cellValues}
+				mode={hardMode}
+				switchMode={handleMode}
+			/>
+
+			<Keyboard onKeyPress={handleKeyPress} cellValues={cellValues} />
+			<WinModal
+				isOpen={isWinModalOpen}
+				onClose={closeWinModalAndResetGame}
+				answer={answer}
+			/>
+			<LoseModal
+				isOpen={isLoseModalOpen}
+				onClose={closeLoseModalAndResetGame}
+				answer={answer}
+			/>
+			<AiWinModal
+				isOpen={isAiWinModalOpen}
+				onClose={closeAiWinModalAndResetGame}
+				answer={answer}
+			/>
+			{isHelpModalOpen && (
+				<InfoModal isOpen={isHelpModalOpen} onClose={closeHelpModal} />
+			)}
+		</div>
+	);
 }
-
 export default App;
---
+++ src/lib/aiBattle.ts
@@ -0,0 +1,36 @@
+import { EASY_WORDS, HARD_WORDS } from '../const/5words';
+
+export const aiGuess = (
+	isHardMode: boolean,
+	turn: number,
+	cellValues: { letter: string; color: string }[][]
+) => {
+	const WORDS = EASY_WORDS.concat(HARD_WORDS);
+
+	if (turn === 1) {
+		return WORDS[Math.floor(Math.random() * WORDS.length)];
+	} else {
+		let filteredWords = WORDS;
+		for (let t = 0; t < turn; t++) {
+			const previousGuess = cellValues[t].map((cell) => cell.letter).join('');
+			const feedbackColors = cellValues[t].map((cell) => cell.color);
+
+			filteredWords = filteredWords.filter((word) => {
+				for (let i = 0; i < feedbackColors.length; i++) {
+					const letter = previousGuess[i];
+					const color = feedbackColors[i];
+					if (color === 'green' && word[i] !== letter) return false;
+					if (
+						color === 'yellow' &&
+						(word[i] === letter || !word.includes(letter))
+					)
+						return false;
+					if (color === 'gray' && word.includes(letter)) return false;
+				}
+				return true;
+			});
+		}
+
+		return filteredWords[Math.floor(Math.random() * filteredWords.length)];
+	}
+};
